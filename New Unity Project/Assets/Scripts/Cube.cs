77b1822169bc52d51d667ca695b29e447afe3316
佐藤幹 <->
2020-03-27 16:51:36 +0900

    情報付け

--- New Unity Project/Assets/Scripts/Cube.cs
+++ New Unity Project/Assets/Scripts/Cube.cs
@@ -7,16 +7,22 @@ public class Cube : MonoBehaviour
     public int turn;
 
     //enumで上がるターンが来たらターンを減らすだけのスクリプト
+    GamePlayManager gameManager;
+
+    Vector3 previous;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameObject.Find("GamePlayManager").GetComponent<GamePlayManager>();
+        previous = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        gameManager.inCubeArray(transform.position,previous);
+
+        previous = transform.position;
     }
 }
